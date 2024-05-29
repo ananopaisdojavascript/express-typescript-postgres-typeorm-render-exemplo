8ae0d0a828f2e67f9d239ae1193057145df708cf
ananopaisdojavascript <ananopaisdojavascript@gmail.com>
2024-05-28 21:05:18 -0300

    render

--- src/server.ts
+++ src/server.ts
@@ -4,9 +4,24 @@ const app = express()
 const port = 3000
 app.use(express.json())
 
+app.get("/users", function (req: Request, res: Response) {
+  res.send('🤗🤗🤗🤗🤗')
+})
 
-app.get('/', (request: Request, response: Response) => {
-  response.send('Hello')
+app.get("/users/:id", function (req: Request, res: Response) {
+  // here we will have logic to return user by id
+})
+
+app.post("/users", function (req: Request, res: Response) {
+  // here we will have logic to save a user
+})
+
+app.put("/users/:id", function (req: Request, res: Response) {
+  // here we will have logic to update a user by a given user id
+})
+
+app.delete("/users/:id", function (req: Request, res: Response) {
+  // here we will have logic to delete a user by a given user id
 })
 
 app.listen(port, () => {
